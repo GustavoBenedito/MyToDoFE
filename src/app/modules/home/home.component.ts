7b3e92330147926c80aea9688c872fd58f81d5f6
GustavoBenedito <gustavobeneditodeveloper@gmail>
2024-07-04 20:22:34 -0300

    MYTODO-2 Create the today component

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SideNavComponent } from "../side-nav/side-nav.component";
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { TodayComponent } from "../today/today.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [SideNavComponent, MatSidenavModule]
+    imports: [SideNavComponent, MatSidenavModule, TodayComponent, CommonModule]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   openSidenav: boolean = true;
+  todayPage: boolean = true;
+  
+  ngOnInit(): void {
+    this.todayPage = true;
+  }
 }
