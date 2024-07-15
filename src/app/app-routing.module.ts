ef40435ec818096cc66bc29aff4ef50f7c7e8d1a
GustavoBenedito <gustavobeneditodeveloper@gmail>
2024-07-14 22:27:53 -0300

    fix route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './modules/add-task/add-task.component';
+import { CompletedComponent } from './modules/completed/completed.component';
+import { HomeComponent } from './modules/home/home.component';
+import { InboxComponent } from './modules/inbox/inbox.component';
+import { TodayComponent } from './modules/today/today.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'addTask', component: AddTaskComponent},
+  { path: 'today', component: TodayComponent},
+  { path: 'inbox', component: InboxComponent},
+  { path: 'today', component: TodayComponent},
+  { path: 'completed', component: CompletedComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodayComponent } from './modules/today/today.component';
+import { HomeComponent } from './modules/home/home.component';
+import { AddTaskComponent } from './modules/add-task/add-task.component';
+import { CompletedComponent } from './modules/completed/completed.component';
+import { InboxComponent } from './modules/inbox/inbox.component';
+import { SidebarComponent } from './modules/sidebar/sidebar.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodayComponent,
+    HomeComponent,
+    AddTaskComponent,
+    CompletedComponent,
+    InboxComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
